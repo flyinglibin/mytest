c2a8e0a9e1c6e38086ad375ec17a246f88f1ceee
Administrator <Administrator@PC-20170725OIYG>
2017-09-01 14:39:08 +0800

    add some profile setting for firefox

--- SeleniumDemo/src/commonObject/UseBrowser.java
+++ SeleniumDemo/src/commonObject/UseBrowser.java
@@ -12,15 +12,16 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class UseBrowser{
 	public WebDriver driver;
 	public WebDriver setupChrome(String test_url){
-//		String path="C:\\Users\\Administrator.PC-20170725OIYG\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe";
+//		String path="C:\\Users\\Administrator.PC-20170725OIYG\\AppData\\Local\\Google\\Chrome\\Application\\chromedriver.exe";
 		String path="E:/drivers/chromedriver.exe";
-		System.setProperty("webdriver.chorme.driver",path);
+		System.setProperty("webdriver.chrome.driver",path);
 		driver = new ChromeDriver();
 		driver.get(test_url);
 		driver.manage().window().maximize();
@@ -28,11 +29,17 @@ public class UseBrowser{
 		return driver;
 	}
 	public WebDriver setupFirefox(String test_url){
-//		String path="D:\\Program Files\\Mozilla Firefox\\firefox.exe";
-//		System.setProperty("webdriver.firefox.bin",path);
-		String path = "E:/drivers/geckodriver.exe";
-		System.setProperty("webdriver.firefox.marionette",path);
-		driver = new FirefoxDriver();
+		String path="D:\\Program Files\\Mozilla Firefox\\firefox.exe";
+		System.setProperty("webdriver.firefox.bin",path);
+		String path1 = "D:\\Program Files\\Mozilla Firefox\\geckodriver.exe";
+		System.setProperty("webdriver.firefox.marionette",path1);
+		
+		FirefoxProfile profile = new FirefoxProfile();
+		profile.setPreference("browser.startup.homepage", "about:blank");
+		profile.setPreference("startup.homepage_welcome_url", "about:blank");
+		profile.setPreference("startup.homepage_welcome_url.additional", "about:blank");
+		
+		driver = new FirefoxDriver(profile);
 		driver.get(test_url);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
